61d8933e0d0579d8e15781fecc326f8ac277ff3b
kokor <kokori.80@mail.ru>
2024-12-31 00:00:56 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+
+        String url = "jdbc:mysql://localhost:3306/mydbtest";
+        String user = "root";
+        String password = "root";
+
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
+
+
